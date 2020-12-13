644573093816968adc984998b0cde4c17b82bc3f
bryancheng66 <bryancheng66@gmail.com>
2020-12-13 18:42:07 -0500

    override methods set and both adds

--- NoNullArrayList.java
+++ NoNullArrayList.java
@@ -8,4 +8,18 @@ public class NoNullArrayList<T> extends ArrayList<T> {
 		}
 		return super.set(index, value);
 	}
+	
+	public void add(int index, T value){
+		if (value == null){
+			throw new IllegalArgumentException("Null cannot be an argument.");
+		}
+		super.add(index, value);
+	}
+
+	public boolean add(T value){
+		if (value == null){
+			throw new IllegalArgumentException("Null cannot be an argument.");
+		}
+		return super.add(value);
+	}
 }
